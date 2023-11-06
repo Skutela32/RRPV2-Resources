9c54043e05266870512713542bd41e6a885e9ecc
Skutela32 <kwebalkop@gmail.com>
2023-11-06 14:38:43 +0000

    Multi Char Fixes

--- [vms]/vms_multichars/config/config_server.lua
+++ [vms]/vms_multichars/config/config_server.lua
@@ -3,18 +3,19 @@ Config.Slots = 3
 Config.EnableStarterItems = true
 Config.StarterItems = {
     {name = 'phone', count = 1},
-    {name = 'welcomebooklet', count = 1},
+    {name = 'sandwich', count = 5},
+    {name = 'water_bottle', count = 5},
 }
 
 Config.EnableStarterMoney = true
 Config.StarterMoney = {
-    {account = 'cash', amount = 350},
-    {account = 'bank', amount = 1750},
+    {account = 'cash', amount = 0},
+    {account = 'bank', amount = 0},
 }
 
 openSpawnSelector = function(src, cData)
     if Config.UseCustomSpawnSelector then
-        TriggerClientEvent('vms_spawnselector:open', src)
+        TriggerClientEvent('qb-spawn:client:openUI', src)
     else
         TriggerClientEvent('apartments:client:setupSpawnUI', src, cData)
     end
