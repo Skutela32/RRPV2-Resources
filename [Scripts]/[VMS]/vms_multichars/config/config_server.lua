be169b61d48a360e4f6003007dd66f9b75588bbb
Skutela32 <kwebalkop@gmail.com>
2023-11-12 02:41:42 +0000

    Swapped Multichar For VMS

---
+++ [Scripts]/[VMS]/vms_multichars/config/config_server.lua
@@ -0,0 +1,22 @@
+Config.Slots = 3
+
+Config.EnableStarterItems = true
+Config.StarterItems = {
+    {name = 'phone', count = 1},
+    {name = 'sandwich', count = 5},
+    {name = 'water_bottle', count = 5},
+}
+
+Config.EnableStarterMoney = true
+Config.StarterMoney = {
+    {account = 'cash', amount = 0},
+    {account = 'bank', amount = 0},
+}
+
+openSpawnSelector = function(src, cData)
+    if Config.UseCustomSpawnSelector then
+        TriggerClientEvent('qb-spawn:client:openUI', src)
+    else
+        TriggerClientEvent('apartments:client:setupSpawnUI', src, cData)
+    end
+end
